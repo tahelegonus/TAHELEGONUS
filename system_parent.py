ce29410db371afd027feab85dc49bbd282861782
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:25:01 -0400

    system_parent.py

---
+++ system_parent.py
@@ -0,0 +1,19 @@
+SYSTEM_PARENT_PROCESSES = {
+
+    "smss.exe",
+
+    "wininit.exe",
+
+    "winlogon.exe",
+
+    "services.exe",
+
+    "lsass.exe",
+
+    "csrss.exe",
+
+    "svchost.exe",
+
+    "explorer.exe",
+
+}
