e5ce85a5fa2898f01159cdaef8ef13c95e82606d
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:18:53 -0400

    security_processes.py

---
+++ security_processes.py
@@ -0,0 +1,11 @@
+SECURITY_PROCESSES = {
+    "lsass.exe",
+    "winlogon.exe",
+    "LogonUI.exe",
+    "smss.exe",
+    "csrss.exe",
+    "wininit.exe",
+    "lsm.exe",
+    "CredentialUIBroker.exe",
+    "consent.exe",
+}
