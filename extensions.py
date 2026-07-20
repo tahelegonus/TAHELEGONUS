7015dd82ddc2c68a79e2e21b789353915e5a51a9
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:08:57 -0400

    extensions.py

---
+++ extensions.py
@@ -0,0 +1,152 @@
+WINDOWS_EXECUTABLE_EXTENSIONS = {
+    ".exe",
+    ".dll",
+    ".sys",
+    ".com",
+    ".scr",
+    ".cpl",
+    ".ocx",
+    ".drv",
+}
+
+WINDOWS_SCRIPT_EXTENSIONS = {
+    ".ps1",
+    ".psm1",
+    ".psd1",
+    ".bat",
+    ".cmd",
+    ".vbs",
+    ".vbe",
+    ".js",
+    ".jse",
+    ".wsf",
+    ".wsh",
+    ".hta",
+}
+
+COMMON_EXTENSIONS = {
+    ".txt",
+    ".csv",
+    ".json",
+    ".xml",
+    ".pdf",
+    ".doc",
+    ".docx",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx",
+}
+
+ARCHIVE_EXTENSIONS = {
+    ".zip",
+    ".rar",
+    ".7z",
+    ".cab",
+    ".tar",
+    ".gz",
+    ".bz2",
+    ".iso",
+}
+
+DOCUMENT_EXTENSIONS = {
+    ".txt",
+    ".pdf",
+    ".doc",
+    ".docx",
+    ".rtf",
+    ".odt",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx",
+    ".csv",
+}
+
+IMAGE_EXTENSIONS = {
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".tif",
+    ".tiff",
+    ".ico",
+    ".svg",
+    ".webp",
+}
+
+VIDEO_EXTENSIONS = {
+    ".mp4",
+    ".avi",
+    ".mov",
+    ".mkv",
+    ".wmv",
+    ".flv",
+    ".mpeg",
+    ".mpg",
+    ".webm",
+}
+
+AUDIO_EXTENSIONS = {
+    ".mp3",
+    ".wav",
+    ".flac",
+    ".aac",
+    ".m4a",
+    ".ogg",
+    ".wma",
+}
+
+BACKUP_EXTENSIONS = {
+    ".bak",
+    ".backup",
+    ".old",
+    ".bkf",
+    ".wbcat",
+    ".vhd",
+    ".vhdx",
+}
+
+LOG_EXTENSIONS = {
+    ".log",
+    ".etl",
+    ".evtx",
+    ".evt",
+    ".trace",
+    ".dmp",
+}
+
+CONFIGURATION_EXTENSIONS = {
+    ".ini",
+    ".cfg",
+    ".conf",
+    ".config",
+    ".yaml",
+    ".yml",
+    ".toml",
+}
+
+DATABASE_EXTENSIONS = {
+    ".db",
+    ".sqlite",
+    ".sqlite3",
+    ".mdb",
+    ".accdb",
+    ".edb",
+}
+
+
+#DATABASE_EXTENSIONS
+
+#CERTIFICATE_EXTENSIONS
+
+#CONFIGURATION_EXTENSIONS
+
+#SOURCE_CODE_EXTENSIONS
+
+#WEB_EXTENSIONS
+
+#EMAIL_EXTENSIONS
+
+#VIRTUAL_MACHINE_EXTENSIONS
