d6b27078d9e7375b88facb24b87d73d569c9ab0b
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:19:46 -0400

    system.py

---
+++ system.py
@@ -0,0 +1,30 @@
+WINDOWS_CORE_PROCESSES = {
+    "smss.exe",
+    "csrss.exe",
+    "wininit.exe",
+    "winlogon.exe",
+    "services.exe",
+    "lsass.exe",
+    "svchost.exe",
+    "spoolsv.exe",
+    "fontdrvhost.exe",
+    "conhost.exe",
+    "dwm.exe",
+    "WmiPrvSE.exe",
+    "dllhost.exe",
+}
+
+
+WINDOWS_USER_PROCESSES = {
+    "explorer.exe",
+    "SearchHost.exe",
+    "StartMenuExperienceHost.exe",
+    "ShellExperienceHost.exe",
+    "RuntimeBroker.exe",
+    "TextInputHost.exe",
+    "ApplicationFrameHost.exe",
+    "LockApp.exe",
+    "taskhostw.exe",
+    "Taskmgr.exe",
+    "SearchIndexer.exe",
+}
