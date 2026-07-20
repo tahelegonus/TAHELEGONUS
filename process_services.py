89a0c59d32a3f8e00eaf47e0a57d795b21ef67b3
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:17:40 -0400

    process_services.py

---
+++ process_services.py
@@ -0,0 +1,24 @@
+WINDOWS_SUSPICIOUS_PATHS = {
+    r"\AppData\Local\Temp",
+    r"\AppData\Roaming",
+    r"\Downloads",
+    r"\Desktop",
+    r"\Public",
+    r"\Recycle.Bin",
+    r"\Temp",
+}
+
+
+WINDOWS_HIGH_RISK_PATHS = {
+    r"\AppData\Roaming",
+    r"\AppData\Local",
+    r"\AppData\Local\Temp",
+    r"\Temp",
+    r"\Downloads",
+    r"\Desktop",
+    r"\Public",
+    r"\Recycle.Bin",
+    r"\Windows\Tasks",
+    r"\Windows\Temp",
+    r"\Startup",
+}
