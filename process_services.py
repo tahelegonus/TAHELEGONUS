f2d73fa17a837df6d5c98e92eefce3dfc1fbb80a
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:11:34 -0400

    process_services.py

---
+++ process_services.py
@@ -0,0 +1,26 @@
+SERVICE_HOST_PROCESSES = {
+    "svchost.exe",
+    "dllhost.exe",
+    "taskhostw.exe",
+    "taskhost.exe",
+    "taskhostex.exe",
+}
+
+
+WINDOWS_SERVICE_PROCESSES = {
+    "services.exe",
+    "svchost.exe",
+    "lsass.exe",
+    "spoolsv.exe",
+    "SearchIndexer.exe",
+    "WmiPrvSE.exe",
+    "sihost.exe",
+    "fontdrvhost.exe",
+    "audiodg.exe",
+    "dasHost.exe",
+    "dwm.exe",
+    "RuntimeBroker.exe",
+    "ctfmon.exe",
+    "winlogon.exe",
+    "wininit.exe",
+}
