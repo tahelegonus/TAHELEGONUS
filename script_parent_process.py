0e1c305b741a80bb34d3d7b12a113451dea1cf6e
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:22:55 -0400

    script_parent_process.py

---
+++ script_parent_process.py
@@ -0,0 +1,17 @@
+SCRIPT_PARENT_PROCESSES = {
+
+    "powershell.exe",
+
+    "pwsh.exe",
+
+    "cmd.exe",
+
+    "wscript.exe",
+
+    "cscript.exe",
+
+    "python.exe",
+
+    "pythonw.exe",
+
+}
