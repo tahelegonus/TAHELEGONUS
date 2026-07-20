806faa3c91b34fd9400fa32857833f750b677289
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:21:52 -0400

    exepcted_parent_processes.py

---
+++ exepcted_parent_processes.py
@@ -0,0 +1,27 @@
+EXPECTED_PARENT_PROCESSES = {
+
+    "cmd.exe": {
+        "explorer.exe",
+        "powershell.exe",
+        "pwsh.exe",
+    },
+
+    "powershell.exe": {
+        "explorer.exe",
+        "cmd.exe",
+        "pwsh.exe",
+    },
+
+    "notepad.exe": {
+        "explorer.exe",
+    },
+
+    "calc.exe": {
+        "explorer.exe",
+    },
+
+    "mspaint.exe": {
+        "explorer.exe",
+    },
+
+}
