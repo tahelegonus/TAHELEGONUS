c2fa1dafc3c51800a8922c60456d5af0284b2a49
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:47:05 -0400

    service_permissions.py

---
+++ service_permissions.py
@@ -0,0 +1,34 @@
+WEAK_SERVICE_DIRECTORIES = {
+    r"C:\Program Files",
+    r"C:\Program Files (x86)",
+    r"C:\ProgramData",
+    r"C:\Tools",
+    r"C:\Apps",
+    r"C:\Software",
+    r"C:\Temp",
+    r"C:\Users\Public",
+}
+
+
+WEAK_SERVICE_DIRECTORIES = {
+    r"C:\Program Files",
+    r"C:\Program Files (x86)",
+    r"C:\ProgramData",
+    r"C:\Tools",
+    r"C:\Apps",
+    r"C:\Software",
+    r"C:\Temp",
+    r"C:\Users\Public",
+}
+
+
+COMMON_SERVICE_HIJACK_PATHS = {
+    r"C:\ProgramData",
+    r"C:\Users\Public",
+    r"C:\Windows\Tasks",
+    r"C:\Windows\Temp",
+    r"\AppData\Local",
+    r"\AppData\Roaming",
+    r"\Startup",
+    r"\Common Startup",
+}
