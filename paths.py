48b3a28b793a5b77144070fa10c249fbc6f0ff87
tahelegonus <telegonuscpt866@gmail.com>
2026-07-20 11:11:08 -0400

    paths.py

---
+++ paths.py
@@ -0,0 +1,53 @@
+WINDOWS_EXPECTED_PATHS = {
+    r"C:\Windows",
+    r"C:\Windows\System32",
+    r"C:\Windows\SysWOW64",
+    r"C:\Program Files",
+    r"C:\Program Files (x86)",
+    r"C:\ProgramData",
+    r"C:\Users",
+}
+
+SYSTEM_DIRECTORIES = {
+    r"C:\Windows",
+    r"C:\Windows\System32",
+    r"C:\Windows\SysWOW64",
+    r"C:\Windows\WinSxS",
+    r"C:\Windows\System",
+    r"C:\Windows\Boot",
+    r"C:\Windows\Fonts",
+    r"C:\Windows\Logs",
+    r"C:\Windows\servicing",
+    r"C:\Program Files",
+    r"C:\Program Files (x86)",
+    r"C:\ProgramData",
+}
+
+
+USER_DIRECTORIES = {
+    r"\Users",
+    r"\Desktop",
+    r"\Documents",
+    r"\Downloads",
+    r"\Pictures",
+    r"\Music",
+    r"\Videos",
+    r"\Favorites",
+    r"\Contacts",
+    r"\Saved Games",
+    r"\Searches",
+    r"\OneDrive",
+    r"\AppData",
+    r"\AppData\Roaming",
+    r"\AppData\Local",
+    r"\AppData\LocalLow",
+}
+
+
+TEMP_DIRECTORIES = {
+    r"\Temp",
+    r"\TMP",
+    r"\Windows\Temp",
+    r"\AppData\Local\Temp",
+    r"\Temporary Internet Files",
+}
