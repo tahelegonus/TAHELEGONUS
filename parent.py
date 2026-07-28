c9e1ae4d7db3818e71e409d9eff5cf4280569e9a
tahelegonus <telegonuscpt866@gmail.com>
2026-07-28 10:59:34 -0400

    parent.py

--- parent.py
+++ parent.py
@@ -1,4 +1,34 @@
-EXPECTED_PROCESS_CHAINS = {
+EXPECTED_WINDOWS_PROCESS_CHAINS = {
+
+    "smss.exe": {
+        "csrss.exe",
+        "wininit.exe",
+    },
+
+    "wininit.exe": {
+        "services.exe",
+        "lsass.exe",
+    },
+
+    "services.exe": {
+        "svchost.exe",
+        "spoolsv.exe",
+    },
+
+    "winlogon.exe": {
+        "userinit.exe",
+        "LogonUI.exe",
+    },
+
+    "userinit.exe": {
+        "explorer.exe",
+    },
+    
+
+}
+
+
+EXPECTED_PROCESS_CHILDREN = {
     "explorer.exe": {
         "cmd.exe",
         "powershell.exe",
@@ -34,31 +64,128 @@ EXPECTED_PROCESS_CHAINS = {
     },
 }
 
-KNOWN_PROCESS_RELATIONSHIPS = {
-    "smss.exe": {
-        "csrss.exe",
-        "wininit.exe",
-    },
-    "wininit.exe": {
-        "services.exe",
-        "lsass.exe",
-        "lsm.exe",
-    },
-    "winlogon.exe": {
-        "userinit.exe",
-    },
-    "userinit.exe": {
+
+
+
+EXPECTED_PARENT_PROCESSES = {
+
+    "cmd.exe": {
         "explorer.exe",
+        "powershell.exe",
+        "pwsh.exe",
+        "conhost.exe",
     },
-    "services.exe": {
-        "svchost.exe",
-        "spoolsv.exe",
+
+    "powershell.exe": {
+        "explorer.exe",
+        "cmd.exe",
+        "pwsh.exe",
     },
-    "explorer.exe": {
+
+    "pwsh.exe": {
+        "explorer.exe",
         "cmd.exe",
         "powershell.exe",
-        "chrome.exe",
-        "msedge.exe",
-        "firefox.exe",
     },
+
+    "conhost.exe": {
+        "cmd.exe",
+        "powershell.exe",
+    },
+
+    "notepad.exe": {
+        "explorer.exe",
+    },
+
+    "calc.exe": {
+        "explorer.exe",
+    },
+
+    "mspaint.exe": {
+        "explorer.exe",
+    },
+
+    "chrome.exe": {
+        "explorer.exe",
+    },
+
+    "msedge.exe": {
+        "explorer.exe",
+    },
+
+    "firefox.exe": {
+        "explorer.exe",
+    },
+
+    "explorer.exe": {
+        "userinit.exe",
+    },
+
+    "svchost.exe": {
+        "services.exe",
+    },
+
+    "spoolsv.exe": {
+        "services.exe",
+    },
+
+    "dllhost.exe": {
+        "svchost.exe",
+        "explorer.exe",
+    },
+
+}
+
+WINDOWS_TRUSTED_PARENT_PROCESSES = {
+
+    "smss.exe",
+
+    "wininit.exe",
+
+    "winlogon.exe",
+
+    "services.exe",
+
+    "lsass.exe",
+
+    "csrss.exe",
+
+    "svchost.exe",
+
+    "explorer.exe",
+
+}
+
+
+
+EXPECTED_OFFICE_CHILDREN = {
+
+    "winword.exe": {
+        "explorer.exe",
+    },
+
+    "excel.exe": {
+        "explorer.exe",
+    },
+
+    "powerpnt.exe": {
+        "explorer.exe",
+    },
+
+}
+
+EXPECTED_BROWSER_PARENTS = {
+
+    "chrome.exe": {
+        "explorer.exe",
+    },
+
+    "msedge.exe": {
+        "explorer.exe",
+    },
+
+    "firefox.exe": {
+        "explorer.exe",
+    },
+
 }
